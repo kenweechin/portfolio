0ddd48aa26083d8e86f0fa7973cd75fe73e3f397
921107136255 <kenweechin@gmail.com>
2023-02-28 23:26:59 +0000

    Added swiper js

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,17 @@ tabs.forEach(tab =>{
         tab.classList.add('qualification__active')
     })  
 })
+
+/*===================================Portfolio Swiper=======================*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
